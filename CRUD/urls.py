e14c0a20b89e05d935ec6b013c03627bc055e2c3
Daniel Silveira <murtdaniel@gmail.com>
2023-01-12 17:59:48 -0300

    [Criação de Update e Delete]

--- CRUD/urls.py
+++ CRUD/urls.py
@@ -22,4 +22,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('form/', views.form, name='form'),
     path('create/', views.create, name='create'),
+    path('view/<int:pk>/', views.view, name='view'),
+    path('edit/<int:pk>/', views.edit, name='edit'),
+    path('update/<int:pk>/', views.update, name='update'),
+    path('delete/<int:pk>/', views.delete, name='delete'),
 ]
--- cadastro/views.py
+++ cadastro/views.py
@@ -17,3 +17,27 @@ def create(request):
     if form.is_valid():
         form.save()
         return redirect('home')
+
+def view(request, pk):
+    data = {}
+    data['db'] = Pessoa.objects.get(pk=pk)
+    return render(request, 'view.html', data)
+
+def edit(request, pk):
+    data = {}
+    data['db'] = Pessoa.objects.get(pk=pk)
+    data['form'] = PessoasModel(instance=data['db'])
+    return render(request, 'form.html', data)
+
+def update(request, pk):
+    data= {}
+    data['db'] = Pessoa.objects.get(pk=pk)
+    form = PessoasModel(request.POST or None, instance=data['db'])
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+
+def delete(request, pk):
+    db = Pessoa.objects.get(pk=pk)
+    db.delete()
+    return redirect('home')
